7e0994a33984bfa8d7b2f7e053fa2a2d781b8680
Thomas Aleknovic <aleknovic70@gmail.com>
2024-01-23 16:34:02 -0300

    initial commit

---
+++ src/main/java/com/thomasaleknovic/workestimateapi/WorkEstimateApiApplication.java
@@ -0,0 +1,13 @@
+package com.thomasaleknovic.workestimateapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkEstimateApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkEstimateApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thomasaleknovic/workestimateapi/WorkEstimateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomasaleknovic.workestimateapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkEstimateApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
