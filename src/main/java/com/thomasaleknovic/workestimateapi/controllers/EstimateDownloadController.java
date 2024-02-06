47b46ebdbb5fcd330c0f6160b1908dfbfe7006e3
Thomas Aleknovic <81420616+thomasaleknovic@users.noreply.github.com>
2024-02-06 14:44:41 +0000

    fix: refactor

--- src/main/java/com/thomasaleknovic/workestimateapi/controllers/EstimateDownloadController.java
+++ src/main/java/com/thomasaleknovic/workestimateapi/controllers/EstimateDownloadController.java
@@ -12,15 +12,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import com.itextpdf.html2pdf.ConverterProperties;
 import com.itextpdf.html2pdf.HtmlConverter;
 import com.lowagie.text.DocumentException;
-import com.thomasaleknovic.workestimateapi.models.Estimate;
 import com.thomasaleknovic.workestimateapi.services.EstimateService;
-import com.thomasaleknovic.workestimateapi.util.PdfGenerator.*;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
