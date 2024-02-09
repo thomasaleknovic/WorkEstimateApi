f29ade70ecc1a4c2b90802a376f5cfca8978acec
Thomas Aleknovic <aleknovic70@gmail.com>
2024-02-09 14:38:52 -0300

    fix: refactor

--- src/main/java/com/thomasaleknovic/workestimateapi/configs/security/SecurityConfig.java
+++ src/main/java/com/thomasaleknovic/workestimateapi/configs/security/SecurityConfig.java
@@ -30,8 +30,10 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/swagger-ui-custom.html").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/swagger-ui/index.html").permitAll()
                         .anyRequest().authenticated())
-                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(this.securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
